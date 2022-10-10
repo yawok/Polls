d2ef1a7d0e329a1b2058533f8dcca93ad09d66b7
yaw.o.k <obengky72@gmail.com>
2022-10-10 13:23:51 +0000

    Add urls, views and templates

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 
-def index(request):
-    """Home page"""
-    return HttpResponse("Hello, world! Welcome to my polling app. Ready to vote?")
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last 5 published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    """Voting page for question choices."""
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {'question': question, "error_message": "You didn't select a choice."})
+    else:
+        # To avoid race condition
+        selected_choice.vote = F("vote") + 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+
 
